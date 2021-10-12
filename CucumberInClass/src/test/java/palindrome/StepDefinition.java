1ffe89093f6b5f1764e284a19fe65771c3484c50
suhasini41 <suhasini.mb@gmail.com>
2021-10-12 13:16:58 -0500

    committed

--- CucumberInClass/src/test/java/palindrome/StepDefinition.java
+++ CucumberInClass/src/test/java/palindrome/StepDefinition.java
@@ -1,15 +1,18 @@
 package palindrome;
 
+import org.junit.Assert;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 
 
+
 public class StepDefinition
 {
 	String s,rs;
-	boolean flag = false;
+	String actualResult = " ";
 	
 	@Given("^a \"([^\"]*)\"$")
 	public void a(String str) throws Throwable {
@@ -24,18 +27,21 @@ public class StepDefinition
 	   rs = sb.reverse().toString();
 	}
 
-	@Then("^I verfiy if they are palindrome or not$")
-	public void i_verfiy_if_they_are_palindrome_or_not() throws Throwable {
+	@Then("^I verfiy if they are equal to the \"([^\"]*)\"$")
+	public void i_verfiy_if_they_are_equal_to_the(String ExpectedResult) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		
-	   if(rs.equals(s))
-	   {
-		   System.out.println("The string '"+s+"' is palindrome");
-	   }
-	   else
-	   {
-		   System.out.println("The string '"+s+"' is not palindrome");
-	   }
+		 if(rs.equals(s))
+		   {
+			   System.out.println("The string '"+s+"' is palindrome");
+			   actualResult = "palindrome";
+			   
+		   }
+		   else
+		   {
+			   System.out.println("The string '"+s+"' is not palindrome");
+			   actualResult = "not palindrome";
+		   }
+		 Assert.assertEquals(actualResult, ExpectedResult);
 	}
 
 }
--- CucumberInClass/src/test/java/palindrome/palindromeString.feature
+++ CucumberInClass/src/test/java/palindrome/palindromeString.feature
@@ -2,11 +2,11 @@ Feature: Check if the given Strings are palindrome
 Scenario Outline: String palindrome verification
 Given a <word>
 When I reverse the String
-Then I verfiy if they are palindrome or not
+Then I verfiy if they are equal to the <result>
 
 Examples:
-|word|
-|"madam"|
-|"phone"|
-|"racecar"|
-|"book"|
+|word|result|
+|"madam"|"palindrome"|
+|"phone"|"not palindrome"|
+|"racecar"|"palindrome"|
+|"book"|"not palindrome"|
